300dc0c2d3087b166203bc78d5713ddabf913e2b
alexander <alexander@klippel.se>
2025-04-10 11:27:44 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -6,7 +6,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://example.com'
+SITEURL = 'https://klppl.github.io/pelican-monoblogg'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
